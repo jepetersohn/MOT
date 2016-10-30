2afd4cbb60f47e494ebf32dcb43ae04b24a8ebcb
alanahanson <jepetersohn@gmail.com>
2016-10-30 18:37:06 -0500

    added some styling

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -6,7 +6,7 @@ post '/sessions' do
   @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:email], params[:password])
       session[:user_id] = @user.id
-      redirect '/channels'
+      redirect "/users/#{@user.id}"
     else
       @message = "Invalid username or password. Please try again."
       erb :"sessions/new"
