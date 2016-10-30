eee734d74631983e50d3be5468dba4744055cd5b
alanahanson <jepetersohn@gmail.com>
2016-10-30 12:09:35 -0500

    added logging in and out, with views, minimal styling

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -6,7 +6,7 @@ post '/sessions' do
   @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:email], params[:password])
       session[:user_id] = @user.id
-      redirect '/entries'
+      redirect '/channels'
     else
       @message = "Invalid username or password. Please try again."
       erb :"sessions/new"
@@ -14,7 +14,7 @@ post '/sessions' do
 
 end
 
-get '/sessions/logout' do
-  session.delete(:user_id)
-  redirect '/entries'
+delete '/sessions' do
+  session[:user_id] = nil
+  redirect '/channels'
 end
