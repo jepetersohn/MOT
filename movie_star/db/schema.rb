be609be11e380d779701a2b53e8549149d2602ad
Justin Frederick <email": null>
2017-01-04 17:39:29 -0600

    add seed database

--- movie_star/db/schema.rb
+++ movie_star/db/schema.rb
@@ -11,9 +11,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170104223053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "genre",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title",       null: false
+    t.integer  "year"
+    t.string   "actors"
+    t.integer  "runtime"
+    t.string   "plot"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.string   "score",      null: false
+    t.integer  "rater_id",   null: false
+    t.integer  "review_id",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "body",        null: false
+    t.integer  "reviewer_id", null: false
+    t.integer  "movie_id",    null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",                         null: false
+    t.string   "email",                            null: false
+    t.string   "hashed_password",                  null: false
+    t.boolean  "trusted_reviewer", default: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+  end
 
 end
