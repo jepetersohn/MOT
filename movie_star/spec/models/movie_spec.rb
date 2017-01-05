3d03ba7a5116fd8f130fc3eca7c078d0f1ae7693
Waleeta C <waleetacanon@gmail.com>
2017-01-05 11:49:10 -0600

    Movie specs passing

--- movie_star/spec/models/movie_spec.rb
+++ movie_star/spec/models/movie_spec.rb
@@ -7,4 +7,19 @@ describe Movie do
     expect(movie.title).to eq "When Harry Met Sally"
   end
 
+  it "should have a year" do
+    expect(movie.year).to eq 1988
+  end
+
+  it "should have actors" do
+    expect(movie.actors).to eq "Billy Crystal, Meg Ryan"
+  end
+
+  it "should have a runtime" do
+    expect(movie.runtime).to eq 189
+  end
+
+  it "should have a plot" do
+    expect(movie.plot).to eq "A friendly couple falls in love"
+  end
 end
