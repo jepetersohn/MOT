7b9a9eb413fcfcd637dca892a5538d60f51cba58
Joan Petersohn <email": null>
2017-01-05 15:25:48 -0600

    got movie list to display posters and retrieve info from api

--- movie_star/app/controllers/movies_controller.rb
+++ movie_star/app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ActionController::Base
   protect_from_forgery with: :exception
   def index
-    #@movies = Movie.all.sort.first(50)
+    @movies = Movie.all.sort.first(50)
   end
 end
