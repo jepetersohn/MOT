3b6caf390748eb63165127c5fc51742a97d166b3
Waleeta C <waleetacanon@gmail.com>
2017-01-06 16:14:35 -0600

    Fixes - for real - trusted_user

--- movie_star/app/helpers/session_helper.rb
+++ movie_star/app/helpers/session_helper.rb
@@ -1,13 +1,22 @@
 module SessionHelper
-  def trusted_reviewer(auth_key)
+  def current_user
+    if active_session?
+      current_user = User.find_by(id: session[:user_id])
+    end
+
+  end
+
+  def is_trusted_reviewer?(auth_key, current_user)
     if auth_key == "987654321"
       current_user.trusted_reviewer = true
+    else
+      current_user.trusted_reviewer = false
     end
   end
 
   def trusted_reviewer?
     if active_session?
-      return true if current_user.trusted_reviewer = true
+      return true if current_user.trusted_reviewer == true
     end
   end
 
@@ -16,12 +25,6 @@ module SessionHelper
       return true if current_user.trusted_reviewer == false
     else
       nil
-    end
-  end
-
-  def current_user
-    if active_session?
-      User.find_by(id: session[:user_id])
     end
   end
 
